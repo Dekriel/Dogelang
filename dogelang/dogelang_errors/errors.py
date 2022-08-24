df46913f3ba7127f4c089fe5b68af434f590a917
Dekriel <92822789+Dekriel@users.noreply.github.com>
2022-08-24 19:09:01 +0100

    e

--- dogelang/dogelang_errors/errors.py
+++
@@ -1,7 +0,0 @@
-def SyntaxFlaw(line, char: int):
-    print("[ERROR]: SyntaxFlaw\n\t- " + line + "\n\t" + " "*(char+1) + "^\nInvalid Syntax :(\n\n- process terminated -")
-
-
-def IllegalChar(line, char: int, text, illchar):
-    print("[ERROR]: IllegalChar, line " + str(line) + "\n\t- " + text + "\n\t" + " "*(char+3) +
-          "^\nIllegal Character \"" + illchar + "\" not accepted." + "\n\n- process terminated -")
